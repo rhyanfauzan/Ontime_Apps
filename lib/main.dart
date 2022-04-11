97a8685d4a57e7e6ee4e5e7565aaadb51961235c
rhyanfauzan <rhyananiki@gmail.com>
2022-04-11 15:35:34 +0700

    by

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:ontime_apps/theme.dart';
 import 'package:ontime_apps/widgets/banner.dart';
 import 'package:ontime_apps/widgets/bottom_nav.dart';
@@ -38,8 +39,37 @@ class MyApp extends StatelessWidget {
                             BannerWidget(),
                             SizedBox(height: 22),
                             HorizontalList(),
-                            SizedBox(height: 30),
+                            SizedBox(height: 33),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'In Progress',
+                                  style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.w600,
+                                    color: whiteColor,
+                                    fontSize: 18
+                                  ),
+                                ),
+                                IconButton(
+                                  icon: Image.asset('assets/images/ArrowLeft.png'),
+                                  iconSize: 7,
+                                  onPressed: () {},
+                                )
+                              ],
+                            ),
+                            SizedBox(height: 23),
                             ListCard(),
+                            SizedBox(height: 50),
+                            Text(
+                                  'By : Ryan NF',
+                                  style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.w400,
+                                    color: lightColor,
+                                    fontSize: 12
+                                  ),
+                                ),
+                            SizedBox(height: 100),
                           ],
                         ),
                       ],
